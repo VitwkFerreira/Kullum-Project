4a25894b266232457ff1cc00002778c1eb2182d5
AquilaMS <aquila.botafogo@gmail.com>
2019-06-27 22:23:49 -0300

    Inicio do projeto
    Commit para criar a organização básica do projeto, já começando com adição dos arquivos .php

---
+++ componentes/nav.php
@@ -0,0 +1,35 @@
+<ul id="slide-out" class="side-nav fixed">
+            <li><div class="user-view">
+              <div class="background">
+                <img src="footer_bg.jpg">
+              </div>
+              <a href="#!user"><img class="circle" src="anon.png"></a>
+              <a href="#!name"><span class="white-text name">Anonymous User</span></a>
+              <a href="#!login"><span class="white-text email">Log in</span></a>
+            </div></li>
+            <li>
+              <div class="row">
+                <div class="col s12">
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <i class="material-icons prefix">textsms</i>
+                      <input type="text" id="autocomplete-input" class="autocomplete">
+                      <label for="autocomplete-input">Buscar por tags</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">code</i>HTML5</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">flare</i>CSS3</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">desktop_mac</i>Bootstrap 3</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">laptop_mac</i>Bootstrap 4</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">dvr</i>Materialize CSS</a></li>
+            <li><div class="divider"></div></li>
+            <li><a class="subheader">Scripts</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">input</i>JavaScript</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">show_chart</i>jQuery</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">local_grocery_store</i>Ajax</a></li>
+            <li><a class="waves-effect" href="#!"><i class="material-icons">local_library</i>AngularJS</a></li>
+        </ul>
+        <a href="#" data-activates="slide-out" class="button-collapse show-on-medium-and-up">
+        	<i class="material-icons indigo" style="color: white;" id="btnSidenav">menu</i></a>
